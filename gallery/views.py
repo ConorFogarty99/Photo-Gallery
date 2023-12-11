f0ea1fa328a1ac752d9eee07a2b1527a061a4bee
ConorFogarty99 <fogartyconor3@gmail.com>
2023-12-11 17:10:54 +0000

    Fix rotation & clean up upload files after upload
    Signed-off-by: ConorFogarty99 <fogartyconor3@gmail.com>

--- gallery/views.py
+++ gallery/views.py
@@ -1,23 +1,20 @@
 import os
 import json
 import boto3
-from dotenv import load_dotenv
+import random
+import traceback
+from PIL import Image
 from .models import Photo
 from django.conf import settings
 from django.shortcuts import render
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib.auth.forms import AuthenticationForm
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from .utils import extract_images_from_zip
-from django.http import JsonResponse
+from django.contrib.auth.forms import AuthenticationForm
 from django.core.files.storage import FileSystemStorage
-from .utils import extract_images_from_zip, cleanup_temp_images
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .utils import extract_images_from_zip, cleanup_temp_images, get_dominant_colors, temp_dir
 from django.views.decorators.csrf import csrf_exempt
-from .utils import get_dominant_colors
-from botocore.exceptions import NoCredentialsError
-import traceback
 
 def is_superuser(user):
     return user.is_superuser
@@ -40,7 +37,8 @@ def login_view(request):
 
 @login_required
 def gallery_view(request):
-    photos = Photo.objects.all()
+    photos = list(Photo.objects.all())
+    random.shuffle(photos)
     return render(request, 'gallery/gallery_view.html', {'photos': photos})
 
 @login_required
@@ -58,7 +56,6 @@ def process_album(request):
     if request.method == 'POST':
         album_zip = request.FILES['album']
         images = extract_images_from_zip(album_zip)
-        # cleanup_temp_images(temp_dir)
         return JsonResponse({'images': images})
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -69,13 +66,18 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 def save_photo_data(request):
     if request.method == 'POST':
         try:
-            print("Enter try")
             data = json.loads(request.body)
             title = data.get('title')
             desc = data.get('description')
             film_type = data.get('film_type')
             camera = data.get('camera')
+            rotation = data.get('rotation', 0)
             local_image_path = os.path.join(settings.MEDIA_ROOT, data.get('imageUrl'))
+
+            if rotation:
+                image = Image.open(local_image_path)
+                image = image.rotate(-rotation, expand=True) # Negative rotation for correct direction
+                image.save(local_image_path) 
 
             s3_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
             s3_image_path = 'albums/' + local_image_path.split('/')[-3] + '/' + local_image_path.split('/')[-1]
@@ -87,6 +89,9 @@ def save_photo_data(request):
             photo = Photo(title=title, description=desc, film_type=film_type, camera=camera, image_url=s3_url, dominant_colors=dominant_colors, album=local_image_path.split('/')[-3])
             photo.save()
 
+            if data.get('is_last_image', False):
+                cleanup_temp_images(temp_dir)
+                
             return JsonResponse({'status': 'success'})
 
         except Exception as e:
