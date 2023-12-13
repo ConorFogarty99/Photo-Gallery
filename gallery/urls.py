5f844e40134b108816eb1fd01592cb131792539f
ConorFogarty99 <fogartyconor3@gmail.com>
2023-12-13 00:31:59 +0000

    Add view album functionality and sort by years
    Signed-off-by: ConorFogarty99 <fogartyconor3@gmail.com>

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import login_view, gallery_view, upload_view, process_album, save_photo_data, delete_photo
+from .views import login_view, gallery_view, upload_view, process_album, save_photo_data, delete_photo, albums_view, album_photos_view, download_album
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -12,4 +12,7 @@ urlpatterns = [
     path('process_album/', process_album, name='process_album'),
     path('save-photo-data/', save_photo_data, name='save_photo_data'),
     path('delete-photo/<int:photo_id>/', delete_photo, name='delete_photo'),
+    path('albums/', albums_view, name='albums'),
+    path('albums/<str:album_name>/', album_photos_view, name='album_photos'),
+    path('download-album/<str:album_name>/', download_album, name='download_album'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gallery/views.py
+++ gallery/views.py
@@ -1,13 +1,16 @@
 import os
+import re
 import json
 import boto3
 import random
 import traceback
+import tempfile
+from zipfile import ZipFile
 from PIL import Image
 from .models import Photo
 from django.conf import settings
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
@@ -15,7 +18,12 @@ from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.decorators import login_required, user_passes_test
 from .utils import extract_images_from_zip, cleanup_temp_images, get_dominant_colors, temp_dir
 from django.views.decorators.csrf import csrf_exempt
+from collections import defaultdict
 from urllib.parse import urlparse
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 def is_superuser(user):
     return user.is_superuser
@@ -58,10 +66,6 @@ def process_album(request):
         album_zip = request.FILES['album']
         images = extract_images_from_zip(album_zip)
         return JsonResponse({'images': images})
-
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 @csrf_exempt
 def save_photo_data(request):
@@ -121,3 +125,48 @@ def delete_photo(request, photo_id):
 
     except Exception as e:
         return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
+    
+@login_required
+def albums_view(request):
+    photos = Photo.objects.all()
+    albums_by_year = defaultdict(set)
+
+    for photo in photos:
+        match = re.search(r'(\d{4})', photo.album)
+        if match:
+            year = match.group(1)
+            album_name = photo.album.replace(year, '').strip()  # Remove year and strip whitespace
+            albums_by_year[year].add((photo.album, album_name))
+
+    # Convert sets to lists for consistent ordering
+    for year in albums_by_year:
+        albums_by_year[year] = sorted(albums_by_year[year], key=lambda x: x[1])
+
+    context = {'albums_by_year': dict(albums_by_year)}
+    return render(request, 'gallery/albums_view.html', context)
+
+@login_required
+def album_photos_view(request, album_name):
+    photos = Photo.objects.filter(album=album_name)
+    return render(request, 'gallery/album_photos_view.html', {'photos': photos, 'album_name': album_name})
+
+def download_album(request, album_name):
+    s3_client = boto3.client('s3')
+    photos = Photo.objects.filter(album=album_name)
+
+    response = HttpResponse(content_type='application/zip')
+    response['Content-Disposition'] = f'attachment; filename="{album_name.replace(" ", "_")}.zip"'
+
+    with ZipFile(response, 'w') as zip_file:
+        for photo in photos:
+            key = photo.image_url.split('amazonaws.com/')[1]  # Extracting the key from the URL
+            print(f"Downloading S3 object with key: {key}")
+            with tempfile.NamedTemporaryFile() as temp_file:
+                try:
+                    s3_client.download_fileobj(settings.AWS_STORAGE_BUCKET_NAME, key, temp_file)
+                    temp_file.seek(0)
+                    zip_file.write(temp_file.name, os.path.basename(key))
+                except Exception as e:
+                    print(f"Failed to download file: {e}")
+
+    return response
